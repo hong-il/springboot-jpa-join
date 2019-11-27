94783b54bf18434ab5877180fc601983b2c35ff6
KIM HONGIL <clroyan01@gmail.com>
2019-11-27 23:10:13 +0900

    Create jpa join project

---
+++ src/main/java/boot/jpa/join/JoinApplication.java
@@ -0,0 +1,13 @@
+package boot.jpa.join;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boot/jpa/join/JoinApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jpa.join;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
