209e92e219279b2ef1d89bf1e688247c9fd11b8b
KIM HONGIL <clroyan01@gmail.com>
2019-11-29 00:35:53 +0900

    Create dto - post

---
+++ src/main/java/boot/jpa/join/dto/post/PostFindAllDto.java
@@ -0,0 +1,28 @@
+package boot.jpa.join.dto.post;
+
+import boot.jpa.join.domain.post.Post;
+import boot.jpa.join.dto.comment.CommentFindAllDto;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class PostFindAllDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private Long user_id;
+    private List<CommentFindAllDto> comments;
+
+    public PostFindAllDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.user_id = post.getUser().getId();
+        this.comments = post.getComments().stream()
+                .map(CommentFindAllDto::new)
+                .collect(Collectors.toList());
+    }
+}
