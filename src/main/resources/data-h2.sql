45f34ad1b042009db9899abf00af0b23936b8706
KIM HONGIL <clroyan01@gmail.com>
2019-12-01 17:39:57 +0900

    Create h2 db - dummy data

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,20 @@
+insert into USER (name, email) values ('hong-il1', 'github.com/hong-il_user1');
+insert into USER (name, email) values ('hong-il2', 'github.com/hong-il_user2');
+
+insert into POST (title, content, user_id) values ('hong-il1', 'github.com/hong-il_post1', '1');
+insert into POST (title, content, user_id) values ('hong-il2', 'github.com/hong-il_post2', '1');
+insert into POST (title, content, user_id) values ('hong-il3', 'github.com/hong-il_post4', '2');
+insert into POST (title, content, user_id) values ('hong-il4', 'github.com/hong-il_post4', '2');
+
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment1', '1', '1');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment2', '1', '1');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment3', '1', '1');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment4', '1', '2');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment5', '1', '2');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment6', '1', '2');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment7', '2', '3');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment8', '2', '3');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment9', '2', '3');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment10', '2', '4');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment11', '2', '4');
+insert into COMMENT (comment, user_id, post_id) values ('github.com/hong-il_comment12', '2', '4');
