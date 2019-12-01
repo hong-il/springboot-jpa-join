66e2e5a404f15a9eaf30a04762a165ce29536267
KIM HONGIL <clroyan01@gmail.com>
2019-12-01 22:46:29 +0900

    Create implicit inner join - user

--- src/main/java/boot/jpa/join/domain/user/UserRepository.java
+++ src/main/java/boot/jpa/join/domain/user/UserRepository.java
@@ -1,6 +1,19 @@
 package boot.jpa.join.domain.user;
 
+import boot.jpa.join.domain.comment.Comment;
+import boot.jpa.join.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u.posts " +
+            "from User u ")
+    List<Post> findAllPost();
+
+    @Query("select u.comments " +
+            "from User u ")
+    List<Comment> findAllComment();
 }
--- src/test/java/boot/jpa/join/domain/user/UserRepositoryTest.java
+++ src/test/java/boot/jpa/join/domain/user/UserRepositoryTest.java
@@ -1,10 +1,19 @@
 package boot.jpa.join.domain.user;
 
+import boot.jpa.join.dto.comment.CommentFindAllDto;
+import boot.jpa.join.dto.post.PostFindAllDto;
 import org.junit.After;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,5 +25,33 @@ public class UserRepositoryTest {
     @After
     public void cleanUp() {
         userRepository.deleteAll();
+    }
+
+    @Test
+    public void ImplicitInnerJoinUserPost() {
+        //given : data-h2.sql
+
+        //when
+        /** select u.posts from User u*/
+        List<PostFindAllDto> posts = userRepository.findAllPost().stream()
+                .map(PostFindAllDto::new)
+                .collect(Collectors.toList());
+
+        //then
+        assertThat(posts.size(), is(4));
+    }
+
+    @Test
+    public void ImplicitInnerJoinUserComment() {
+        //given : data-h2.sql
+
+        //when
+        /** select u.comments from User u*/
+        List<CommentFindAllDto> comments = userRepository.findAllComment().stream()
+                .map(CommentFindAllDto::new)
+                .collect(Collectors.toList());
+
+        //then
+        assertThat(comments.size(), is(12));
     }
 }
