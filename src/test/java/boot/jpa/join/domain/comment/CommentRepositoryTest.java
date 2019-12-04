f2a4bb52463d7a3809146125c9d6f24636835882
KIM HONGIL <clroyan01@gmail.com>
2019-12-04 19:08:28 +0900

    Create repository test - comment

---
+++ src/test/java/boot/jpa/join/domain/comment/CommentRepositoryTest.java
@@ -0,0 +1,20 @@
+package boot.jpa.join.domain.comment;
+
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentRepositoryTest {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @After
+    public void cleanUp() {
+        commentRepository.deleteAll();
+    }
+}
