93ba808b8a85ccd5672939b575f6cc28978d1300
KIM HONGIL <clroyan01@gmail.com>
2019-12-04 19:15:26 +0900

    Create multiple join - comment

--- src/main/java/boot/jpa/join/domain/comment/CommentRepository.java
+++ src/main/java/boot/jpa/join/domain/comment/CommentRepository.java
@@ -1,6 +1,16 @@
 package boot.jpa.join.domain.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("select c " +
+            "from Post p " +
+            "JOIN p.user u " +
+            "JOIN p.comments c " +
+            "WHERE u.name IS NOT NULL")
+    List<Comment> MultipleJoinUserComment();
 }
--- src/test/java/boot/jpa/join/domain/comment/CommentRepositoryTest.java
+++ src/test/java/boot/jpa/join/domain/comment/CommentRepositoryTest.java
@@ -1,10 +1,18 @@
 package boot.jpa.join.domain.comment;
 
+import boot.jpa.join.dto.comment.CommentFindAllDto;
 import org.junit.After;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,5 +24,20 @@ public class CommentRepositoryTest {
     @After
     public void cleanUp() {
         commentRepository.deleteAll();
+    }
+
+    @Test
+    public void MultipleJoinUserComment() {
+        //given : data-h2.sql
+
+        //when
+        /** select c from Post p JOIN p.user u JOIN p.comments c WHERE u.name IS NOT NULL*/
+        List<CommentFindAllDto> comments = commentRepository.MultipleJoinUserComment().stream()
+                .map(CommentFindAllDto::new)
+                .collect(Collectors.toList());
+
+        //then
+        System.out.println();
+        assertThat(comments.size(), is(12));
     }
 }
